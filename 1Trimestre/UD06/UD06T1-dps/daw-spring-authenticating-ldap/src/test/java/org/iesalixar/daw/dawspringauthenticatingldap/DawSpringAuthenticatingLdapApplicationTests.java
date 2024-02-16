fc27bad474577e4a003223c765483378d9f24fe6
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-02-16 22:09:06 +0100

    ud05t2

---
+++ 1Trimestre/UD06/UD06T1-dps/daw-spring-authenticating-ldap/src/test/java/org/iesalixar/daw/dawspringauthenticatingldap/DawSpringAuthenticatingLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw.dawspringauthenticatingldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawSpringAuthenticatingLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
